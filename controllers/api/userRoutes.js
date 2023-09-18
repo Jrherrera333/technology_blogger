7c702418ff2366db52703dc1f8102f0a9fa1cbd5
Jose Herrera <herrera.joserafael@gmail.com>
2023-09-18 17:48:36 -0400

    fix the login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -50,8 +50,21 @@ router.post('/logout', (req, res) => {
   }
 });
 
-router.post('/user', (req, res) => {
-console.log(req);
-res.status(200).end();
+router.post('/', async (req, res) => {
+  try {
+    console.log(req.body);
+    const userData = await User.create(req.body);
+    console.log(userData);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
 module.exports = router;
